f816fd3946befc8b72cd8eddacc5e4c008f7b3ac
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-11-14 10:27:11 +0100

    fix(index): fix missing options assignment

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
+// ------------------------------------
+// #PUG - INDEX
+// ------------------------------------
+
+const assign = Object.assign
+
 const compile = require('pug').compile
 const parse = require('posthtml-parser')
 
 /**
+ * @author Michael Ciniawsky (@michael-ciniawsky)
+ * @description Pug Parser for PostHTML
+ * @license MIT
+ *
  * @module posthtml-pug
  * @version 1.0.0
- * @author Michael Ciniawsky (@michael-ciniawsky)
- *
- * @desc Pug Parser for PostHTML
  *
  * @requires pug
  * @requires posthtml-parser
- *
- * @method posthtmlPug
  *
  * @param  {String} pug     Pug Source
  * @param  {Object} options Pug Options
  *
  * @return {Function} HTML
  */
-module.exports = function posthtmlPug (options) {
-  options.pretty = options.pretty || true
-  options.locals = options.locals || {}
+module.exports = function (options) {
+  options = assign({ pretty: true, locals: {} }, options)
 
-  return function (pug) {
-    return parse(compile(pug, options)(options.locals))
+  /**
+   * @method pug
+   *
+   * @param  {String} src File
+   *
+   * @return {Object}     PostHTML Tree
+   */
+  return function pug (src) {
+    return parse(compile(src, options)(options.locals))
   }
 }
